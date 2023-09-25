2f197391ccf8514fcf5fba60f60f88fd8f9fb24a
Francois.leon <francois.leon@ubisoft.com>
2023-09-25 18:07:52 +0200

    Change WAMhelper library load method and bump version to 0.0.3
    The load method has been change

---
+++ source/prefix.ps1
@@ -0,0 +1,2 @@
+﻿# This is required to expose in a private way the cmdlet Get-WAMToken which is required when we use the -WAMFlow
+import-module $(Join-Path $PSScriptRoot 'lib' 'WAMHelper.dll') -ErrorAction Stop
